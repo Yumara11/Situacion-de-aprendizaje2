62d897c8dbf8d56c7487d9e7e55f2814af2cc350
Yumara Chacon Soto <ychasot2009@g.educaand.es>
2025-02-21 11:52:51 +0100

    Add files via upload

---
+++ Actividad 6/js/script.js
@@ -0,0 +1,14 @@
+function paquito() {
+    let valorintroducido;
+    valorintroducido= document.getElementById("texto"). value;
+    let resto;
+
+    resto= valorintroducido % 2;
+
+        if(resto ==0) {
+            document.getElementById("parrafo").innerHTML = "el número es par";
+        }
+        else{
+            document.getElementById("parrafo").innerHTML ="el número es impar";
+        }
+}
