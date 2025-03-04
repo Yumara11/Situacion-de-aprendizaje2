0efc3163df9af1b8cfbf7e3b93826725703e982f
Yumara Chacon Soto <ychasot2009@g.educaand.es>
2025-03-04 19:34:20 +0100

    Update script.js

--- Actividad 6/js/script.js
+++ Actividad 6/js/script.js
@@ -1,6 +1,6 @@
 function paquito() {
     let valorintroducido;
-    parseInt valorintroducido= document.getElementById("texto"). value;
+    valorintroducido=parseInt(document.getElementById("texto"). value;
     let resto;
 
     resto= valorintroducido % 2;
